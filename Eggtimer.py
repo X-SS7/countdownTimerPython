4c8f4c4c6639de82276b4f82b0df9c1b2829129e
X-SS7 <sakshyamsigdel@gmail.com>
2023-04-08 13:50:50 +0545

    Update Eggtimer.py
    Added time in minutes function for convinience

--- Eggtimer.py
+++ Eggtimer.py
@@ -2,9 +2,9 @@ import time
 import pyttsx3
 
 engine = pyttsx3.init()
+time_in_minutes = int(input("Time in minutes: "))
 
-time_in_seconds = int(input("Time in seconds: "))
-
+time_in_seconds = time_in_minutes * 60
 for i in range(time_in_seconds, 0, -1):
     print(i)
     time.sleep(1)
